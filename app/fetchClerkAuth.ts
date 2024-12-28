940f69c894794f83a8ba322d142f4d47db19ea22
Sebastian Cabeza <sebastian.cabeza@noted.com>
2024-12-28 09:53:48 -0300

    simplify auth

--- app/fetchClerkAuth.ts
+++ app/fetchClerkAuth.ts
@@ -5,7 +5,7 @@ import { createClerkClient } from "@clerk/backend";
 
 export const fetchClerkAuth = createServerFn({ method: "GET" }).handler(
   async () => {
-    const { userId } = await getAuth(getWebRequest());
+    const { userId, has } = await getAuth(getWebRequest(), {});
 
     const clerkClient = createClerkClient({
       secretKey: process.env.CLERK_SECRET_KEY,
@@ -13,9 +13,17 @@ export const fetchClerkAuth = createServerFn({ method: "GET" }).handler(
 
     const user = userId ? await clerkClient.users.getUser(userId) : null;
 
+    const isAdmin = userId
+      ? has({ permission: "org:catalog:manage" })
+      : undefined;
+
     return {
-      userId,
-      userEmail: user?.emailAddresses.at(0)?.emailAddress,
+      user: user
+        ? {
+            id: userId,
+            isAdmin: has({ permission: "org:catalog:manage" }),
+          }
+        : undefined,
     };
   }
 );
--- app/routes/admin/catalog.tsx
+++ app/routes/admin/catalog.tsx
@@ -1,27 +1,34 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { SignIn } from "@clerk/tanstack-start";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useOrganizationList } from "@clerk/tanstack-start";
 import { fetchClerkAuth } from "../../fetchClerkAuth";
-import { getServerTime } from "../../getServerTime";
-import {
-  SignedIn,
-  SignOutButton,
-  SignedOut,
-  SignInButton,
-} from "@clerk/tanstack-start";
+import { SignOutButton, SignInButton } from "@clerk/tanstack-start";
 
 export const Route = createFileRoute("/admin/catalog")({
   loader: async () => {
-    const { userEmail } = await fetchClerkAuth();
-    const serverTime = await getServerTime();
-
-    return { userEmail, serverTime };
+    const { user } = await fetchClerkAuth();
+    if (!user) {
+      throw Error("Not authenticated");
+    }
+    if (!user.isAdmin) {
+      throw Error("Not authorized");
+    }
+    return user;
   },
   component: RouteComponent,
   errorComponent: ({ error }) => {
     if (error.message === "Not authenticated") {
       return (
-        <div className="flex items-center justify-center p-12">
-          <SignIn routing="hash" forceRedirectUrl={window.location.href} />
+        <div>
+          <p>You are signed out</p>
+          <SignInButton />
+        </div>
+      );
+    }
+    if (error.message === "Not authorized") {
+      return (
+        <div>
+          <div>Not authorized</div>
+          <SignOutButton />
         </div>
       );
     }
@@ -31,19 +38,17 @@ export const Route = createFileRoute("/admin/catalog")({
 });
 
 function RouteComponent() {
-  const state = Route.useLoaderData();
+  const { setActive } = useOrganizationList();
+
+  setActive?.({ organization: "delasartes" });
+
   return (
     <div>
-      <SignedIn>
-        <div>Hello {state.userEmail}</div>
-        <div>Server time: {state.serverTime}</div>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <p>You are signed out</p>
-
-        <SignInButton />
-      </SignedOut>
+      <div>Catalog</div>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <SignOutButton />
     </div>
   );
 }
